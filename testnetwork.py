783b8c97960ad0d35e2f3b7b898aac6ef7d3949b
William Zou <nuqiewuoz@gmail.com>
2018-07-20 15:27:31 +0800

    update test network

--- testnetwork.py
+++ testnetwork.py
@@ -23,38 +23,61 @@ def test_network(repeat=20, api=None):
         api_url = api
 
     url = base_url+api_url
+    diffs = []
 
     with requests.Session() as s:
         # call url once to complete 3 hand shakes.
         s.get(url)
         beginat = time.time()
-        logging.info('begin url connect test:{}'.format(url))
+        lprint('begin network connect test:{}'.format(url), logging.DEBUG)
         for i in range(repeat):
             old = time.time()*1000
             r = s.get(url)
             now = time.time()*1000
-            lprint("before:{} after:{} delta:{}ms".format(old, now, int(now-old)), logging.DEBUG)
-            lprint("local:{} server:{} delta:{}ms".format(now, r.json()[
-                   'data'], int(now)-int(r.json()['data'])), logging.DEBUG)
+            server = int(r.json()['data'])
+            diff = (now + old)/2 - server
+            lprint("before:{} after:{} server:{} delta:{}ms difference:{}".format(old, now, server, int(now-old), int(diff)), logging.DEBUG)
+            diffs.append(diff)
+
         endat = time.time()
-        lprint('total time:{:.5}s avg time:{:.4}ms'.format(
-            endat-beginat, (endat-beginat)/repeat*1000))
+        lprint('total time:{:.5}s avg time:{:.4}ms'.format(endat-beginat, (endat-beginat)/repeat*1000))
+    # 返回本地和服务器的时间差, 单位毫秒
+    return sum(diffs)/len(diffs)
 
 
-def test_create_order(repeat = 5):
+def test_create_order(repeat = 10):
     fcoin = Fcoin(api_key, api_secret)
     beginat = time.time()
-    logging.info('begin buy test:')
+    symbol = "fteth"
+    # price = fcoin.get_market_price(symbol)
+    price = 0.00001
+    logging.info('begin create order test: buy {}'.format(symbol))
+    orders_info = []
     for i in range(repeat):
         old = time.time()*1000
-        buy_result = fcoin.buy("ethusdt", 520.00, 0.001)
+        buy_result = fcoin.buy(symbol, price, 3)
         now = time.time()*1000
         lprint("before:{} after:{} delta:{}ms".format(
             old, now, int(now-old)), logging.DEBUG)
-        lprint("buy order result:{}".format(buy_result['data']))
+        logging.debug("buy order result:{}".format(buy_result))
+        orders_info.append((buy_result['data'], old))
     endat = time.time()
     lprint('total time:{:.5}s avg time:{:.4}ms'.format(
         endat-beginat, (endat-beginat)/repeat*1000))
+    # 计算从下单开始，到系统记录下单之间的时间
+    delta_times = []
+    for order_id, call_time in orders_info:
+        order = fcoin.get_order(order_id)
+        delta = int(order['data']['created_at']) - call_time
+        # cancel the test order
+        fcoin.cancel_order(order_id)
+        delta_times.append(delta)
+    avg = sum(delta_times)/len(delta_times)
+    # 估算时间差
+    system_delta = test_network()
+    lprint('average time delta: {:.5}ms differece:{:.4}ms'.format(
+        avg, system_delta))
+    lprint('平均下单时间为:{:.4}ms'.format(avg+system_delta))
 
 
 if __name__ == '__main__':
