ce5e5966c60cbd68ec7c5adb1edf3405335ae33a
William Zou <nuqiewuoz@gmail.com>
2018-07-31 11:07:05 +0800

    add ft btc pair

---
+++ ft_btc_triangular_config.py
@@ -0,0 +1,14 @@
+sleepsecond = 0.5  # 每单结束后的休息时间
+s1 = 'ft'
+s2 = 'btc'
+s3 = 'usdt'
+# 为了减少计算量，避免单位换算
+_s2amount = 0.0005
+_s1amount = 25  # 0.01*2500
+# 超过halfs1和halfs2，就可以下单，默认为一半
+halfs1 = _s1amount/2
+halfs2 = _s2amount/2
+
+# 套利比例，1.001意味着1‰
+difference = 1.001
+is_use_amount = True
